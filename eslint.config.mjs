8375b354cf20fb65d88e4fd24aa7f98c74786d9c
codingsh <codingsh@pm.me>
2025-04-21 18:32:46 +0100

    chore(eslint): remove eslint ignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  
+  {
+    ignores: [    
+      "src/hooks/useChainSubscriptions.ts",      
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "public/**"
+    ]
+  }
 ];
 
 export default eslintConfig;
